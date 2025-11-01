368d60e0d0f3b61c9c4de415e8f9bb5eac859800
arbbnk96-del <arbbnk96@gmail.com>
2025-11-01 19:12:53 +0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import { io } from "socket.io-client";
 import Login from "./Login";
 
 // export const serverRoute = "http://localhost:8080";
-export const serverRoute = "https://abshrak-se-kzwt.onrender.com";
+export const serverRoute = "https://absherak-se-jhf4.onrender.com";
 export const token = localStorage.getItem("token");
 export const socket = io(serverRoute);
 function App() {
